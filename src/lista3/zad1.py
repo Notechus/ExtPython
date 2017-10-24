39a633437b327869a6cc96be5b85ce651ff86ebd
notechus <seba0713@gmail.com>
2017-10-24 20:42:28 +0200

    lista3

--- src/lista3/zad1.py
+++ src/lista3/zad1.py
@@ -1,20 +1,21 @@
 from math import sqrt, ceil
-
-
-def is_prime(x):
-    for i in range(2, ceil(sqrt(x))):
-        if x % i == 0:
-            return False
-    return True
+from time import time
 
 
 def pierwsze_funkcyjna(n):
-    return list(filter(is_prime, range(n + 1)))
+    return list(filter(lambda x: all(not x % i == 0 for i in range(2, ceil(sqrt(x)))), range(2, n + 1)))
 
 
 def pierwsze_skladana(n):
-    return [x for x in range(n + 1) if is_prime(x)]
+    return [x for x in range(2, n + 1) if all(x % i != 0 for i in range(2, ceil(sqrt(x))))]
 
 
-print(pierwsze_funkcyjna(20))
-print(pierwsze_skladana(20))
+start1 = time()
+print(pierwsze_funkcyjna(2000))
+end1 = time()
+start2 = time()
+print(pierwsze_skladana(2000))
+end2 = time()
+
+print(end1 - start1)
+print(end2 - start2)
--- src/lista3/zad2.py
+++ src/lista3/zad2.py
@@ -1,21 +1,21 @@
 from math import ceil
-
-
-def is_perfect(x):
-    suma = 0
-    for i in range(1, ceil(x / 2) + 1):
-        if x % i == 0:
-            suma += i
-    return suma == x
+from time import time
 
 
 def doskonale_funkcyjna(n):
-    return list(filter(is_perfect, range(n + 1)))
+    return list(filter(lambda x: sum(i for i in range(1, ceil(x / 2) + 1) if x % i == 0) == x, range(6, n + 1)))
 
 
 def doskonale_skladana(n):
-    return [x for x in range(n + 1) if is_perfect(x)]
+    return [x for x in range(6, n + 1) if sum(i for i in range(1, ceil(x / 2) + 1) if x % i == 0) == x]
 
 
+start1 = time()
 print(doskonale_funkcyjna(10000))
+end1 = time()
+start2 = time()
 print(doskonale_skladana(10000))
+end2 = time()
+
+print(end1 - start1)
+print(end2 - start2)
